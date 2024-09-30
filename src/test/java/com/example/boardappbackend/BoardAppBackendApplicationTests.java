749246539c335360f411d5acaf268c4ff1fcd6a1
bit\bitcamp <tlma@naver.com>
2024-09-30 10:27:41 +0900

    backend

--- src/test/java/com/example/boardappbackend/BoardAppBackendApplicationTests.java
+++ src/test/java/com/example/boardappbackend/BoardAppBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.boardappbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BoardAppBackendApplicationTests {
 
     @Test
